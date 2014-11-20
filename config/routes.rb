3d2244cfc1e19f29b5223d82258a112c7b45c89c
YANKE Guo <me@yanke.io>
2014-11-21 04:17:27 +0800

    adding bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+
+  root 'home#index'
+
   devise_for :users
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
