f8770300738075ae8388a11d8ac58cc1f22c79ae
YANKE Guo <me@yanke.io>
2014-11-23 07:26:52 +0800

    optinize

--- lib/extensions.rb
+++ lib/extensions.rb
@@ -1,9 +1,6 @@
 # Extend Sanitize with cleaning hash values
 class Sanitize
   def self.clean_hash(hash, config)
-    hash.each {|key, value|
-      hash[key] = self.clean value, config
-    }
-    hash
+    hash.each { |key, value| hash[key] = self.clean value, config }
   end
 end
