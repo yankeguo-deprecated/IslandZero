7c95e7c83baa369acae123efea06b7e0d7afadb1
YANKE Guo <me@yanke.io>
2014-11-23 09:46:06 +0800

    bugfix

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
          :confirmable, :lockable, :timeoutable, :async
+
+  before_save do
+    self.nickname = self.email.split('@').first if self.nickname.nil?
+  end
 end
