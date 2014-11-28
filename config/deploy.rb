71daef7fc014476700b11ac37c3dd91585f52f55
YANKE Guo <me@yanke.io>
2014-11-29 06:35:44 +0800

    update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -29,7 +29,7 @@ set :sidekiq_role,      -> { :worker }
 # set :linked_files, fetch(:linked_files, []).push('config/database.yml')
 
 # Default value for linked_dirs is []
-# set :linked_dirs, fetch(:linked_dirs, []).push('bin', 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
+set :linked_dirs, fetch(:linked_dirs, []).push('bin', 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
