13ff543288b7bfeccd372299796c126d578ceb9d
YANKE Guo <me@yanke.io>
2014-11-30 03:23:33 +0800

    add empty rspecs

---
+++ spec/controllers/posts_controller_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe PostsController, :type => :controller do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
---
+++ spec/controllers/topics_controller_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe TopicsController, :type => :controller do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
