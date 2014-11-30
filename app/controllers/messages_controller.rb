e2dbe1ae9143169c472b9121b5dba0c3e940ef83
YANKE Guo <me@yanke.io>
2014-12-01 02:59:50 +0800

    add logic prevent same content in short period. #3.

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -17,6 +17,21 @@ class MessagesController < ApplicationController
       return
     end
 
+    # Check Duplicated Message
+    msg_hash = Digest::SHA1.hexdigest(post_params[:content])
+    lock_key = "lock.dm.#{current_user.id}"
+    if $redis.setnx(lock_key, msg_hash)
+      $redis.expire(lock_key, 3)
+    else
+      if request.xhr?
+        render plain: t(:pls_no_duplicated_message), status: 400
+      else
+        flash.alert = t(:pls_no_duplicated_message)
+        redirect_to :back
+      end
+      return
+    end
+
     # Create model
     @message = current_user.messages.create(post_params)
 
