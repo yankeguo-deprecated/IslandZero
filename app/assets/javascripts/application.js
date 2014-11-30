98007673786deb826e427142810bd120c3eab0fb
YANKE Guo <me@yanke.io>
2014-12-01 05:04:50 +0800

    fix bootstrap-markdown with markdown.js and turbolinks page:load

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,6 +14,7 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require bootstrap
+//= require markdown
 //= require bootstrap-markdown
 //= require bootstrap-markdown.zh
 //= require_tree ./shared
