ab9b53163c6a748cb1178d6cca75938705139401
YANKE Guo <me@yanke.io>
2014-11-30 09:07:56 +0800

    add i18n for time ago

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,7 @@
 class Message < ActiveRecord::Base
+  # Include DataHelper so we can use time_ago_in_words
+  include ActionView::Helpers::DateHelper
+
   # Belongs to chattable
   belongs_to :chattable, polymorphic: true, inverse_of: :messages
 
@@ -11,7 +14,7 @@ class Message < ActiveRecord::Base
       user_id:        self.user.id,
       user_nickname:  self.user.nickname,
       content:        self.content,
-      created_at:     self.created_at.to_s
+      created_at:     time_ago_in_words(self.created_at)
     }
     RealtimeChatController.publish("/chat/#{self.chattable_type}/#{self.chattable_id}", hash)
     true
