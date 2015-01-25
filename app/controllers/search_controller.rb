75ee804b38f990c31965b42b4013eaac23ffe72e
YANKE Guo <me@yanke.io>
2015-01-25 21:41:41 +0800

    bugfix for empty search

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -4,6 +4,7 @@ class SearchController < ApplicationController
     if params[:keyword].blank?
       flash.alert = t(:keyword_should_not_be_empty)
       redirect_to :back
+      return
     end
     keyword = "%#{params[:keyword]}%"
     @topics = Topic.where("title LIKE ? OR introduction LIKE ?", keyword, keyword)
